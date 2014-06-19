b0f3dc6c00920d5efa9e853d6a98176a2f794632
Brian Jenkins <bonkydog@bonkydog.com>
2014-06-19 13:35:27 -0700

    Ask user for name, store in database.

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
                  [compojure "1.1.6"]
                  [hiccup "1.0.5"]
                  [ring-server "0.3.1"]
-                 [ring/ring-servlet "1.2.2"]]
+                 [ring/ring-servlet "1.2.2"]
+                 [org.clojure/java.jdbc "0.3.2"]
+                 [postgresql "9.1-901.jdbc4"]]
   :plugins [[lein-ring "0.8.10"]]
   :min-lein-version "2.0.0"
   :ring {:handler hw.handler/app}
--- src/hw/handler.clj
+++ src/hw/handler.clj
@@ -1,10 +1,42 @@
 (ns hw.handler
   (:use compojure.core)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [ring.adapter.jetty :as jetty]
+            [clojure.java.jdbc :as sql]
+            [hiccup.core :refer :all]))
+
+(def db (or (System/getenv "DATABASE_URL")
+              "postgresql://localhost:5432/hw"))
+
+;; heroku addons:add heroku-postgresql:dev
+;; heroku pg:psql
+;; CREATE TABLE names (id SERIAL, name TEXT);
+
+(defn ask-name []
+  (html [:html
+         [:head [:title "Hi!"]]
+         [:body
+          [:p "What is your name?"]
+          [:form {:action "/" :method "POST"}
+           [:input {:type "text" :name "user_name"}]
+           [:input {:type "submit"}]]]]))
+
+(defn greet [params]
+  (sql/insert! db :names {:name (:user_name params)})
+  (str "Hello, " (:user_name params)))
+
+(defn show-log []
+  (let [entries (sql/query db ["select * from names"])]
+    (html [:p "Here are people who stopped by:"]
+          [:table
+           [:tbody
+            (map (fn [e] [:tr [:td (:name e)]]) entries)]])))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (ask-name))
+  (POST "/" {params :params} (greet params))
+  (GET "/log" []  (show-log))
   (route/resources "/")
   (route/not-found "Not Found"))
 
