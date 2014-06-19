77991ef12cabc2445fab38c598ff36fca47b1262
Brian Jenkins <bonkydog@bonkydog.com>
2014-06-19 13:11:03 -0700

    Adjust project file for Heroku.

--- project.clj
+++ project.clj
@@ -1,9 +1,13 @@
 (defproject hw "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.6"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.1.6"]
+                 [hiccup "1.0.5"]
+                 [ring-server "0.3.1"]
+                 [ring/ring-servlet "1.2.2"]]
   :plugins [[lein-ring "0.8.10"]]
+  :min-lein-version "2.0.0"
   :ring {:handler hw.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
